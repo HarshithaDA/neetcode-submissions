5a21fd49923d694b2e3d86a40e1602c96a2831fe
Harshitha Devina Anto <73550067+HarshithaDA@users.noreply.github.com>
2026-09-02 10:57:25 -0700

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        # for each value in target triplet, check if and filter out the triplets that have values greater than the target values
+        # if equal to target values, 
+        # if all len(target) values have been got, return true
+        
+        # positions we complete from target
+        # return true if this equals len(target)
+        good = set()
+
+        # iterate input list
+        for t in triplets:
+            # greater than target values, filter out
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+
+            # this triplet could contain values we are looking for
+            # if value at position i is equal to target at i 
+            for i,v in enumerate(t):
+                if v == target[i]:
+                    good.add(i)
+
+        return len(good) == 3
+            

0d5209b10e7f8fadb5ba056cb1993a16a6e190b7
Harshitha Devina Anto <73550067+HarshithaDA@users.noreply.github.com>
2026-09-05 12:14:22 -0700

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+
+        # solution o(1) time & space
+        # check only the 1s - n = n & (n-1)
+        ones = 0
+        while n>0:
+            n = n & (n-1)
+            ones +=1
+
+        return ones
+
+
+
+        # solution o(1) time & space
+        # checking if rightmost digit is 1 or 0 -> modulo with 2
+        # if ans is 1 then its 1 else its 0 -> 1%2=1 0%2=0
+        # how to check other digits? - bit shift operation shift all to right
+        # ones = 0
+
+        # while n>0:
+        #     # check if ones place is 0 or not
+        #     # if its a 1 increment 0
+        #     if n%2 == 1:
+        #         ones+=1
+        #     # shift everything to the right by 1
+        #     n = n >> 1
+
+        # return ones
+
